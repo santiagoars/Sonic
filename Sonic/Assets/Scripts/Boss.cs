88374beec6f44f6c29fce4312718da36545fc27b
lChrios <a01634500@gmail.com>
2019-11-30 21:34:07 -0600

    chris

--- Sonic/Assets/Scripts/Boss.cs
+++ Sonic/Assets/Scripts/Boss.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
+    public int stage;
+    public GameObject player;
+    public BossStats[] phases;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.stage = 0;
+
+        //Estadisticas fase 1
+        phases[0] = new BossStats(15, 5.0f, 10.0f, 2.0f, true);
+
+        //Estadisticas fase 2
+        phases[1] = new BossStats(3, 25.0f, 30.0f, 1.0f, false);
+
+        //Estadisticas fase 3
+        phases[2] = new BossStats(10, 10.0f, 15.0f, 3.0f, true);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    private void damage()
+    {
+        this.phases[stage].health--;
+        if(this.phases[stage].health == 0)
+        {
+            this.stage++;
+
+            // Aquí iría script si se necesita cambiar la animación
+
+
+
+
+            yield return new WaitForSeconds(this.phases[stage].stunTime * 2);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player"))
+        {
+            damage();
+        }
     }
 }
---
+++ Sonic/Assets/Scripts/BossStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class BossStats
+{
+    public bool canThrow = false;
+    public int health = 6; 
+    public float jumpHeight = 10.0f,
+                 movSpeed = 5.0f,
+                 stunTime = 1.5f;
+
+    public BossStats(int h, float j, float m, float s, bool c)
+    {
+        this.health = h;
+        this.jumpHeight = j;
+        this.movSpeed = m;
+        this.stunTime = s;
+        this.canThrow = c;
+    } 
+
+}
